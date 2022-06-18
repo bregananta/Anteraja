19ea6d22a1a0afbde52a90e546a839b1f9ea826b
Kunto Bregananta Adi <bregananta@gmail.com>
2022-06-18 10:29:37 +0700

    default config to empty string

--- config/anteraja.php
+++ config/anteraja.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'anterajaBasePath' => env('ANTERAJA_BASEPATH', 'https://doit-sit.anteraja.id/sembilansuksesindonesia'),
-    'anterajaAccessKeyId' => env('ANTERAJA_ACCESS_KEY_ID', 'Anteraja_x_sembilansuksesindonesia_SIT'),
-    'anterajaSecretAccessKey' => env('ANTERAJA_SECRET_ACCESS_KEY', 'SYRzSiaro+a9U0wa5munOw==')
+    'anterajaBasePath' => env('ANTERAJA_BASEPATH', ''),
+    'anterajaAccessKeyId' => env('ANTERAJA_ACCESS_KEY_ID', ''),
+    'anterajaSecretAccessKey' => env('ANTERAJA_SECRET_ACCESS_KEY', '')
 ];
